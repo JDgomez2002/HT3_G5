fe5a8a37608bf17590fd8ae5aeefbdd6e95f85c5
lourdessaa11 <saa21333@uvg.edu.gt>
2022-02-10 19:39:17 -0600

    Update TestOrdenamientoTest.java

--- TestOrdenamientoTest.java
+++ TestOrdenamientoTest.java
@@ -9,6 +9,60 @@
 //Actividad: Hoja de Trabajo 3 
 //GRUPO 5
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class TestOrdenamientoTest {
-    
+
+	//variables
+	RadixSort r = new RadixSort();
+	QuickSort q = new QuickSort();
+	MergeSort m = new MergeSort();
+	BubbleSort b = new BubbleSort();
+	GnomeSort g = new GnomeSort();
+	
+	int[] i = {1,4,7,3,6,2,5};
+	
+	int[] orden = {1,2,3,4,5,6,7};
+
+	//pruebas de cada método principal
+	@Test
+	void testRadixSort() {
+		
+		r.radixSort(i);
+		assertArrayEquals(orden, i);
+	}
+	
+	@Test
+	void testGnomeSort() {
+		
+		g.gnomeSort(i);
+		assertArrayEquals(orden,i);
+	}	
+
+	@Test
+	void testBubbleSort() {
+		
+		b.bubbleSort(i);
+		assertArrayEquals(orden,i);
+	}
+	
+	@Test
+	void testMergeSort() {
+		
+		m.mergeSort(i, 7);
+		assertArrayEquals(orden,i);
+	}
+	
+	@Test
+	void testQuickSort() {
+		
+		q.quickSort(i, 7);
+		assertArrayEquals(orden,i);
+	}
 }
+
